a38c60bea2a28a70a3a0654280d92ae4f10659a4
tilla.bakos <tilla.bakos@gmail.com>
2017-09-29 12:42:34 +0200

    adding index.html

--- server.py
+++ server.py
@@ -1,12 +1,16 @@
-from flask import Flask, render_template, redirect, request, url_for
+from flask import Flask, render_template, redirect, request, url_for, session
 
 app = Flask(__name__)
 
 
 @app.route('/')
-def hello_world():
-    return 'hello world'
+def main_page():
+    return render_template('index.html')
 
+
+@app.route('/memory')
+def game_page():
+    return render_template('game.html')
 
 if __name__ == '__main__':
     app.secret_key = "b'\x95=\xb1\t\xb2\x8eL\r\xed\x17\x07MQ\xf5\x86\xb3\xac\xed<\x92\x05Z\xb6\xae'"
